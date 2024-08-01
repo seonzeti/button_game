c7a4bfafafeff18449b37486a47ec35b5174a6c4
seonzeti <86936140+seonzeti@users.noreply.github.com>
2024-08-01 17:05:31 +0900

    Add files via upload
    upload

---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', (e) => {
+    // console.log('keydown event', e.key)
+
+    if (e.key == '1' || e.key == '2' || e.key == '3') {
+        const div = document.getElementById(e.key)
+        div.classList.add('pressed')
+    }
+})
+
+
+window.addEventListener('keyup', (e) => {
+    if (e.key == '1' || e.key == '2' || e.key == '3') {
+        const div = document.getElementById(e.key)
+        div.classList.remove('pressed')
+    }
+})
+
